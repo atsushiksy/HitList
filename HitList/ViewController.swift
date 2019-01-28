a4a24d45d1d332f7cf20be3e2616000bfdc8cb51
Atsushi Kasuya <atsushi@jakaz.com>
2019-01-27 23:30:22 -0800

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Atsushi Kasuya on 1/27/19.
+//  Copyright © 2019 Atsushi Kasuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
